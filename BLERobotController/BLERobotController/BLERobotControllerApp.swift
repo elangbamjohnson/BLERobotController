e2088f2fc9072ee782185a5c197bc44e71a517b3
Johnson Elangbam <elangbamjohnson@gmail.com>
2026-06-02 10:47:04 +0530

    Add User Interface project

---
+++ BLERobotController/BLERobotController/BLERobotControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLERobotControllerApp.swift
+//  BLERobotController
+//
+//  Created by Johnson Elangbam on 02/06/26.
+//
+
+import SwiftUI
+
+@main
+struct BLERobotControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BLERobotController/BLERobotController/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BLERobotController
+//
+//  Created by Johnson Elangbam on 02/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
